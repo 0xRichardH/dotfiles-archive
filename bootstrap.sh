278a2105a4625cd4601cb694c8cf1fa6c8469b16
Richard Hao <xilu@hey.com>
2021-08-07 04:22:28 +0000

    Refactor -> rcm

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-require_relative "./src/install_rcm"
+require_relative "./src/rcm"
 
 RCM_VERSION = "1.3.4".freeze
 DOTFILES = "~/dotfiles".freeze
@@ -13,7 +13,7 @@ puts "-> 2. Clone haoxilu/dotfiles-local onto the machine"
 system "git clone git://github.com/haoxilu/dotfiles-local.git #{DOTFILES_LOCAL}"
 
 puts "-> 3. Install rcm"
-InstallRcm.new(version: RCM_VERSION).install!
+Rcm.new(version: RCM_VERSION).install!
 
 puts "-> 4. Create symlinks for config files in your home director"
 system "env RCRC=$HOME/dotfiles/rcrc rcup"
--- src/rcm.rb
+++ src/rcm.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class InstallRcm
+class Rcm
   attr_reader :version
 
   def initialize(version:)
