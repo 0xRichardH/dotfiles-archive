7b3bffa5ce18b8f06ba1e982e7af76617ea1ca0e
Richard Hao <xilu@hey.com>
2021-08-07 04:20:02 +0000

    run rcup to create symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,17 @@
 require_relative "./src/install_rcm"
 
 RCM_VERSION = "1.3.4".freeze
+DOTFILES = "~/dotfiles".freeze
+DOTFILES_LOCAL = "~/dotfiles-local".freeze
 
 puts "-> 1. Clone thoughtbot/dotfiles onto the machine"
-system "git clone git://github.com/thoughtbot/dotfiles.git ~/dotfiles"
+system "git clone git://github.com/thoughtbot/dotfiles.git #{DOTFILES}"
 
 puts "-> 2. Clone haoxilu/dotfiles-local onto the machine"
-system "git clone git://github.com/haoxilu/dotfiles-local.git ~/dotfiles-local"
+system "git clone git://github.com/haoxilu/dotfiles-local.git #{DOTFILES_LOCAL}"
 
 puts "-> 3. Install rcm"
 InstallRcm.new(version: RCM_VERSION).install!
 
-# Run rcup
+puts "-> 4. Create symlinks for config files in your home director"
+system "env RCRC=$HOME/dotfiles/rcrc rcup"
