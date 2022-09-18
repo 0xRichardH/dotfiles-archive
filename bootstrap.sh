21ee6a7045fdaef8e1e3c8cb90d7ff69c1c39106
Richard Hao <xilu@hey.com>
2022-09-18 22:54:08 +0800

    feat: remove the commentted code

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,22 +18,18 @@ DOTFILES_LOCAL_DIR = File.join(HOME_PATH, "dotfiles-local").freeze
 DOTFILES_GIT_REPO = "https://github.com/thoughtbot/dotfiles.git".freeze
 DOTFILES_LOCAL_GIT_REPO = "https://github.com/haoxilu/dotfiles-local.git".freeze
 
-# if ENV["CI"].nil?
-#   # ENV["CI"] = "true"
-# end
-
 # Pre-check
 puts "-> 0. Pre-checking"
-# Prerequirement.new(dotfiles_dir: DOTFILES_DIR, dotfiles_local_dir: DOTFILES_LOCAL_DIR).check!
+Prerequirement.new(dotfiles_dir: DOTFILES_DIR, dotfiles_local_dir: DOTFILES_LOCAL_DIR).check!
 
 puts "-> 1. Install the system requirements."
-# Requirements.new.install!
+Requirements.new.install!
 
 puts "-> 2. Clone thoughtbot/dotfiles onto the machine"
-# Git.new(repo: DOTFILES_GIT_REPO, path: DOTFILES_DIR).clone!
+Git.new(repo: DOTFILES_GIT_REPO, path: DOTFILES_DIR).clone!
 
 puts "-> 3. Clone haoxilu/dotfiles-local onto the machine"
-# DotfilesLocal.new(repo: DOTFILES_LOCAL_GIT_REPO, path: DOTFILES_LOCAL_DIR).clone!
+DotfilesLocal.new(repo: DOTFILES_LOCAL_GIT_REPO, path: DOTFILES_LOCAL_DIR).clone!
 
 puts "-> 4. Setup Plugins"
 Plugins.new(local: DOTFILES_LOCAL_DIR, home: HOME_PATH).install!
